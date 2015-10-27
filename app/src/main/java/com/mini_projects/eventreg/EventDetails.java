ebe766e4c5c1b6d8258c09c919c88fdbdb376d41
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-27 15:37:26 +0300

    Event Details: Activity and Layout Created - Header Created

---
+++ app/src/main/java/com/mini_projects/eventreg/EventDetails.java
@@ -0,0 +1,18 @@
+package com.mini_projects.eventreg;
+
+/**
+ * Created by iWanjugu on 27/10/2015.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_details);
+    }
+}
