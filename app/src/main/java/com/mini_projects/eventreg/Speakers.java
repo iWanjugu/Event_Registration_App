d7da75687fb2e7d41ba3bfdda461fab5b3f3a6cc
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-28 06:02:11 +0300

    Speakers Activity and Layout Added

---
+++ app/src/main/java/com/mini_projects/eventreg/Speakers.java
@@ -0,0 +1,16 @@
+package com.mini_projects.eventreg;
+
+/**
+ * Created by iWanjugu on 28/10/2015.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class Speakers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speakers);
+    }
+}
+
