c8276966dba7616678309fbd929fe32bb0c683ec
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-28 06:10:02 +0300

    OnClickListener set to the 'Speakers' tab linking to the Speaker activity

--- app/src/main/java/com/mini_projects/eventreg/EventDetails.java
+++ app/src/main/java/com/mini_projects/eventreg/EventDetails.java
@@ -3,16 +3,34 @@ package com.mini_projects.eventreg;
 /**
  * Created by iWanjugu on 27/10/2015.
  */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
 
-public class EventDetails extends AppCompatActivity {
+
+public class EventDetails extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_details);
+
+    // Creating on click change activities for a button
+    GridLayout firstGridLayout = (GridLayout)findViewById(R.id.gridLayout);
+    firstGridLayout.setOnClickListener(this);
+    }
+    private void grid1click ()
+    {
+        startActivity(new Intent("android.intent.action.SPEAKERS"));
+    }
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case  R.id.gridLayout:
+                grid1click();
+                break;
+        }
     }
 }
