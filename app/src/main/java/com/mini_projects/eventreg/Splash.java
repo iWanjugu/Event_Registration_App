efd3b0c4d74bc4826cda9489e4aa67028835c6d9
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-29 20:45:36 +0300

    Splash: Activity and Layout created, image added, and linked to 'Main.java' Activity

---
+++ app/src/main/java/com/mini_projects/eventreg/Splash.java
@@ -0,0 +1,50 @@
+package com.mini_projects.eventreg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+//    To make the image rotate on the splash screen
+//    New imageview variable
+    final ImageView splashImageView = (ImageView) findViewById(R.id.splashImage);
+
+//    New Rotation variable
+    final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+    final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+
+        splashImageView.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                splashImageView.startAnimation(an2);
+                finish();
+                Intent i = new Intent("android.intent.action.MAIN");
+                startActivity (i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
+
