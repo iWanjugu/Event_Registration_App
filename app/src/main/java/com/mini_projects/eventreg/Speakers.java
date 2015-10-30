b0c16645994721b06412785ef6bf538a80a5f824
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-30 23:34:53 +0300

    Speakers: Attempt to implement Cards using RecycleView and Adapters in MDL; not yet successful

--- app/src/main/java/com/mini_projects/eventreg/Speakers.java
+++ app/src/main/java/com/mini_projects/eventreg/Speakers.java
@@ -5,12 +5,61 @@ package com.mini_projects.eventreg;
  */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 public class Speakers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.speakers);
+        /* Create and customize RecyclerView. */
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Add 5 cards
+        MyAdapter adapter = new MyAdapter(new String[5]);
+        recyclerView.setAdapter(adapter);
     }
+
+    /* Create RecylcerView Adapter. */
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public View view;
+            public TextView title;
+            public ViewHolder(View v) {
+                super(v);
+                view = v;
+                title = (TextView) v.findViewById(R.id.card_title);
+            }
+        }
+
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View cardview = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.cardview, parent, false);
+            return new ViewHolder(cardview);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.title.setText("Card " + (position + 1));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
 }
 
