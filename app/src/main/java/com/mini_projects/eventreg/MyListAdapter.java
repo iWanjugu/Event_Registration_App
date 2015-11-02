e415a32b0e2e749a5f71f790679c81ecbe55fe03
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-02 10:37:39 +0300

    MyListAdapter Adapter for ListView template 2

---
+++ app/src/main/java/com/mini_projects/eventreg/MyListAdapter.java
@@ -0,0 +1,33 @@
+package com.mini_projects.eventreg;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class MyListAdapter extends ArrayAdapter<String> {
+
+    public MyListAdapter(Context context, String[] values) {
+        super(context, R.layout.listview_row_layout_2);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater listInflater = LayoutInflater.from(getContext());
+
+        View theView = listInflater.inflate(R.layout.listview_row_layout_2,
+                parent, false);
+
+        String infoItem = getItem(position);
+        TextView theTextView = (TextView) theView.findViewById(R.id.textViewTemplate2);
+        theTextView.setText(infoItem);
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.imageViewTemplate2);
+        theImageView.setImageResource(R.drawable.wifi_50);
+
+        return theView;
+    }
+}
