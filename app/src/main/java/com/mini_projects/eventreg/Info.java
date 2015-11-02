ffcdc472c91dfaaa2c5942e53f1ecdf3f795f90c
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-02 04:56:20 +0300

    Info: List view with simple layout 1 -  Method 2  (works better thatn the first)

--- app/src/main/java/com/mini_projects/eventreg/Info.java
+++ app/src/main/java/com/mini_projects/eventreg/Info.java
@@ -2,42 +2,77 @@ package com.mini_projects.eventreg;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class Info extends ListActivity {
-    String[] names = {"WIFI", "VENUE", "PAYMENT", "TRANSPORT", "CONTACTS"};
+public class Info extends AppCompatActivity {
+//    // METHOD 1
+//
+//    //the list names
+//    //formula 1:
+//    //String[] names = {"WIFI", "VENUE", "PAYMENT", "TRANSPORT", "CONTACTS"};
+//
+//    //formula 2: Using Array defined in 'strings.xml'
+//    String[] names;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //setContentView(R.layout.info);
+//
+//
+//        //Array Adapter is responsible for
+//            //collecting the variables in string to list view
+//            //applying styles to the list view
+//        names = getResources().getStringArray(R.array.info_array);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, names);
+//            //adapter = name of adapter object
+//            // android.R.layout.simple_list_item_1 = styling
+//            //names - database which will be displayed in the list View
+//        // Adding adapter object to List View
+//        setListAdapter(adapter);
+//    }
+//    // another method - when item in list view is clicked
+//    public void onListItemClick(ListView l, View v, int position, long id)
+//    {
+//        Toast.makeText(getBaseContext(), "You have clicked " + names[position], Toast.LENGTH_LONG).show();
+//    }
 
+    //METHOD 2
+    //Using Array defined in 'strings.xml'
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.info);
-        //Array Adapter is responsible for
-            //collecting the variables in string to list view
-            //applying styles to the list view
+        setContentView(R.layout.info);
+        String[] names;
+        names = getResources().getStringArray(R.array.info_array);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
-            //adapter = name of adapter object
-            // android.R.layout.simple_list_item_1 = styling
-            //names - database which will be displayed in the list View
-        // Adding adapter object to List View
-        setListAdapter(adapter);
-    }
-    // another method - when item in list view is clicked
-    public void onListItemClick(ListView l, View v, int position, long id)
-    {
-        Toast.makeText(getBaseContext(), "You have clicked " +names[position], Toast.LENGTH_LONG).show();
-    }
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                names);
+        ListView theListView = (ListView)findViewById(R.id.infoList);
+        theListView.setAdapter(theAdapter);
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
+        //catching clicks
+        theListView.setOnItemClickListener(new
+           AdapterView.OnItemClickListener() {
+               @Override
+               public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                    String ListItemPicked = "You clicked " +
+                      String.valueOf(adapterView.getItemAtPosition(position));
+                   Toast.makeText(Info.this, ListItemPicked, Toast.LENGTH_SHORT).show();
+               }
+           });
+
+
+    }
 
 }
