417626ec705a4def218fa7ae0442eec29f8252c8
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-09 04:26:54 +0300

    Activity with card views - unsuccesssful

--- app/src/main/java/com/mini_projects/eventreg/Speakers.java
+++ app/src/main/java/com/mini_projects/eventreg/Speakers.java
@@ -2,16 +2,59 @@ package com.mini_projects.eventreg;
 
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Speakers extends Activity {
+
+    private MyCardAdapter adapter = new MyCardAdapter(this,getData());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.speakers);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
     }
+
+    public static List<CardViewInfo> getData() {
+        List<CardViewInfo> data = new ArrayList<>();
+        int[] icons = {
+                R.drawable.wifi_50,
+                R.drawable.arena_50,
+                R.drawable.coins_50,
+                R.drawable.shuttle_50,
+                R.drawable.phone_50,
+        };
+
+        String[] names = {
+                "WIFI",
+                "VENUE",
+                "PAYMENT",
+                "TRANSPORT",
+                "CONTACTS"
+        };
+
+        for (int i = 0; i < names.length && i < icons.length; i++) {
+            CardViewInfo current = new CardViewInfo();
+            current.iconId = icons[i];
+            current.title = names[i];
+            data.add(current);
+        }
+        return data;
+    }
+
 }
 
 
